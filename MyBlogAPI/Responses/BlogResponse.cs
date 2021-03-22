bf4a7285efce2cf1ec9ab41208acfeb39bc2cfb8
Vianney Doleans <vianney.doleans@epitech.eu>
2021-03-22 21:46:01 +0100

    wip : pagination in progress

---
+++ MyBlogAPI/Responses/BlogResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlogAPI.Errors
+{
+    public class BlogResponse<T>
+    {
+        public BlogResponse(T data)
+        {
+            Data = data;
+        }
+
+        public T Data { get; set; }
+    }
+}
---
+++ MyBlogAPI/Responses/PagedBlogResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace MyBlogAPI.Responses
+{
+    public class PagedBlogResponse<T> : BlogResponse<T>
+    {
+        public int Offset { get; set; }
+        public int Limit { get; set; }
+        public int Total { get; set; }
+
+        public PagedBlogResponse(T data, int offset, int limit) : base(data)
+        {
+            Offset = offset;
+            Limit = limit;
+            Data = data;
+        }
+    }
+}
