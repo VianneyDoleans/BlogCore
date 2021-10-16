34f5bcfb0a02c5cf81070bde7703d944e9cedb05
Vianney Doleans <vianney.doleans@epitech.eu>
2021-10-16 22:41:48 +0200

    feat(): add postgreSQL database provider - Entity Framework

--- DbAccess/ServiceCollectionExtensions.cs
+++ DbAccess/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using DbAccess.DataContext;
+﻿using System;
+using DbAccess.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,7 +11,22 @@ namespace DbAccess
         public static IServiceCollection RegisterDataServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<MyBlogContext>(o =>
-                o.UseSqlServer(configuration.GetConnectionString("Default")));
+            {
+                var dbType = configuration["DatabaseType"];
+                switch (dbType)
+                {
+                    case "SQLServer":
+                        o.UseSqlServer(
+                            configuration.GetConnectionString("Default"));
+                        break;
+                    case "PostgreSQL":
+                        o.UseNpgsql(
+                            configuration.GetConnectionString("Default"));
+                        break;
+                    default:
+                        throw new Exception("Unsupported database provider : " + dbType);
+                }
+            });
 
             return services;
         }
