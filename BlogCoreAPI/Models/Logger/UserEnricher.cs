ae937ae1a838682e824e80d0745a1f16182e7457
Vianney DOLEANS <vianney.doleans@gmail.com>
2022-09-25 20:47:37 +0200

    chore: clean comment inside userEnricher (serialog)

--- BlogCoreAPI/Models/Logger/UserEnricher.cs
+++ BlogCoreAPI/Models/Logger/UserEnricher.cs
@@ -13,8 +13,8 @@ namespace BlogCoreAPI.Models.Logger
         {
         }
 
-        //Dependency injection can be used to retrieve any service required to get a user or any data.
-        //Here, I easily get data from HTTPContext
+        // Dependency injection can be used to retrieve any service required
+        // Here HTTPContext is injected to get the user information
         public UserEnricher(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
@@ -24,10 +24,8 @@ namespace BlogCoreAPI.Models.Logger
         {
             var userName = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Name) ?? "anonymous";
             var userId = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier) ?? "N/A";
-            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
-                "Username", userName));
-            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
-                "UserId", userId));
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("Username", userName));
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty("UserId", userId));
         }
     }
 }
