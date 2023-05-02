0b08e451d9c3d1e8db4dab9db375bab1db9e300c
Vianney DOLEANS <vianney.doleans@gmail.com>
2023-05-03 00:33:20 +0200

    fix: fix tests related to user identity

---
+++ BlogCoreAPI.FunctionalTests/Models/EmailServiceMock.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BlogCoreAPI.Models.Mails;
+using BlogCoreAPI.Services.MailService;
+
+namespace BlogCoreAPI.FunctionalTests.Models;
+
+public class EmailServiceMock : IEmailService
+{
+    public Task SendEmailAsync(Message message, CancellationToken token)
+    {
+        return Task.CompletedTask;
+    }
+}
