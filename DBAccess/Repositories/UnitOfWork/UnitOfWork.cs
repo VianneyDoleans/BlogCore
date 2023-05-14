ca3fcde0ae945e6031da52640414b8e92a187eb7
Vianney DOLEANS <vianney.doleans@gmail.com>
2023-05-14 19:15:46 +0200

    wip: add check inside unit of work to update modified entities property

--- DBAccess/Repositories/UnitOfWork/UnitOfWork.cs
+++ DBAccess/Repositories/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,8 @@
-﻿using DBAccess.DataContext;
+﻿using System;
+using System.Linq;
+using DBAccess.Contracts;
+using DBAccess.DataContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace DBAccess.Repositories.UnitOfWork
 {
@@ -13,9 +17,45 @@ namespace DBAccess.Repositories.UnitOfWork
             _context.Dispose();
         }
 
+        
+        private void ChangeModifiedAt()
+        {
+            var added = _context.ChangeTracker.Entries()
+                .Where(t => t.Entity is IHasModificationDate && t.State == EntityState.Added)
+                .Select(t => t.Entity)
+                .ToArray();
+            
+            foreach (var entity in added)
+            {
+                if (entity is IHasModificationDate track)
+                {
+                    track.ModifiedAt = DateTimeOffset.UtcNow;
+                }
+            }
+        }
+        
+        private void ChangeLastLogin()
+        {
+            var added = _context.ChangeTracker.Entries()
+                .Where(t => t.Entity is IHasLastLogin && t.State == EntityState.Added)
+                .Select(t => t.Entity)
+                .ToArray();
+            
+            foreach (var entity in added)
+            {
+                if (entity is IHasLastLogin track)
+                {
+                    track.LastLogin = DateTimeOffset.UtcNow;
+                }
+            }
+        }
+        
         /// <inheritdoc />
         public void Save()
         {
+            ChangeModifiedAt();
+            ChangeLastLogin();
+
             _context.SaveChanges();
         }
 
